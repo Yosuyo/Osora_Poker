c8d5075995bbffeb15cdefbe72fe299caf5568b3
yasuya <porepore1996@gmail.com>
2019-03-12 23:09:45 +0900

    基本的には完成しているがバグあり また一部のカードの判定が不十分で画像範囲を広げる必要がある

--- main.py
+++ main.py
@@ -19,10 +19,12 @@ spade = ["s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11", "s1
 
 #カードの検出とハンドリスト作成
 def check():
+    global hand
     sample = Image.open(R"G:\AutoPoker\data\JOKER.png")
     try:
-        x, y = pgui.locateCenterOnScreen(sample, region=(20, 380, 450, 170), confidence=0.980)
+        x, y = pgui.locateCenterOnScreen(sample, region=(35, 400, 370, 55), confidence=0.980, grayscale=True)
         hand.append([x, y, 0, 0])
+        print("JOKER")
     except:
         pass
 
@@ -30,11 +32,13 @@ def check():
     for name in club:
         if len(hand) == 5:
             break
+        
         sample = f"G:\AutoPoker\data\{name}.png"
         search = Image.open(sample)
         try:
-            x, y = pgui.locateCenterOnScreen(search, region=(20, 380, 450, 170), confidence=0.980)
+            x, y = pgui.locateCenterOnScreen(search, region=(35, 400, 370, 55), confidence=0.980, grayscale=True)
             hand.append([x, y, 1, n])
+            print(name)
         except:
             pass
         n += 1
@@ -47,8 +51,9 @@ def check():
         sample = f"G:\AutoPoker\data\{name}.png"
         search = Image.open(sample)
         try:
-            x, y = pgui.locateCenterOnScreen(search, region=(20, 380, 450, 170), confidence=0.980)
+            x, y = pgui.locateCenterOnScreen(search, region=(35, 400, 370, 55), confidence=0.980, grayscale=True)
             hand.append([x, y, 2, n])
+            print(name)
         except:
             pass
         n += 1
@@ -61,8 +66,9 @@ def check():
         sample = f"G:\AutoPoker\data\{name}.png"
         search = Image.open(sample)
         try:
-            x, y = pgui.locateCenterOnScreen(search, region=(20, 380, 450, 170), confidence=0.980)
+            x, y = pgui.locateCenterOnScreen(search, region=(35, 400, 370, 55), confidence=0.980, grayscale=True)
             hand.append([x, y, 3, n])
+            print(name)
         except:
             pass
         n += 1
@@ -75,19 +81,21 @@ def check():
         sample = f"G:\AutoPoker\data\{name}.png"
         search = Image.open(sample)
         try:
-            x, y = pgui.locateCenterOnScreen(search, region=(20, 380, 450, 170), confidence=0.980)
+            x, y = pgui.locateCenterOnScreen(search, region=(35, 400, 370, 55), confidence=0.980, grayscale=True)
             hand.append([x, y, 4, n])
+            print(name)
         except:
             pass
         n += 1
 
 #ジョーカー判定
 def joker():
+    global hand
     for card in hand:
         if card[2] == 0:
-            print("joker exists")
-            pgui.click(num[0], num[1])
-            hand.remove(num)
+            pgui.click(card[0], card[1])
+            hand.remove(card)
+            global JOKER
             JOKER = 1
             break
 
@@ -97,6 +105,7 @@ def flush():
     di = [2,0]
     he = [3,0]
     sp = [4,0]
+    suit.clear()
     suit = [cl, di, he, sp]
     p = 4
     if JOKER == 1:
@@ -112,11 +121,11 @@ def flush():
             sp[1] += 1
     for n in suit:
         if n[1] >= p:
-            print("flush " + n[1] + "枚 " + n[0])
             for card in hand:
                 if n[0] == card[2]:
                     pgui.click(card[0], card[1])
                     time.sleep(0.3)
+            global FIN
             FIN = 1
             break
                 
@@ -146,17 +155,126 @@ def pair():
                 if card[3] == num[0]:
                     pgui.click(card[0], card[1])
                     time.sleep(0.3)
+            global FIN
             FIN = 1
-        
 
+#選択セクション     
+def choice():
+    check() #存在するカードの情報をhandに加える
+    joker() #ジョーカーが存在した場合handから削除し、JOKERフラグを立てる
+    flush() #フラッシュ期待ハンドの対応
+    if FIN == 0:
+        pair()  #スリーペア期待ハンドの対応
+    start()
 
-#リストとフラグ
+#High and Lowセクション
+def highlow():
+    num = 0
+    for name in range(2, 15):
+        ad = f"G:\AutoPoker\data\{name}.png"
+        numImage = Image.open(ad)
+        try:
+            pgui.locateOnScreen(numImage, region=(120, 365, 40, 40), confidence=0.900, grayscale=True)
+            print("High and Low", name)
+            num = name
+            break
+        except:
+            pass
+    if num == 0:
+        pass
+    elif 1 <= num <= 8:
+        print("HIGH")
+        no()
+    elif 9 <= num <= 14:
+        print("LOW")
+        yes()
+    
+#指示判定と対応
+def order():
+    ORDER = 0
+    if ORDER == 0:
+        hold = Image.open(R"G:\AutoPoker\data\hold.png")
+        try:
+            pgui.locateOnScreen(hold, region=(120, 310, 260, 50), confidence=0.950, grayscale=True)
+            print("ホールドするカードを選択")
+            choice()
+            ORDER = 1
+        except:
+            pass
+    if ORDER == 0:
+        restart = Image.open(R"G:\AutoPoker\data\restart.png")
+        try:
+            pgui.locateOnScreen(restart, region=(120, 310, 260, 50), confidence=0.950, grayscale=True)
+            print("STARTで再挑戦できます")
+            start()
+            ORDER = 1
+        except:
+            pass
+    if ORDER == 0:
+        highOrLow = Image.open(R"G:\AutoPoker\data\highOrLow.png")
+        try:
+            pgui.locateOnScreen(highOrLow, region=(75, 280, 350, 50), confidence=0.950, grayscale=True)
+            print("HighかLowを選んでください")
+            highlow()
+            ORDER = 1
+        except:
+            pass
+    if ORDER == 0:
+        doubleUp = Image.open(R"G:\AutoPoker\data\doubleUp.png")
+        try:
+            pgui.locateOnScreen(doubleUp, region=(110, 310, 270, 50), confidence=0.950, grayscale=True)
+            print("ダブルアップに挑戦しますか？")
+            yes()
+            ORDER = 1
+        except:
+            pass
+    if ORDER == 0:
+        Continue = Image.open(R"G:\AutoPoker\data\continue.png")
+        try:
+            pgui.locateOnScreen(Continue, region=(110, 275, 270, 50), confidence=0.950, grayscale=True)
+            print("続けて挑戦しますか？")
+            yes()
+            ORDER = 1
+        except:
+            pass
+    if ORDER == 0:
+        restart = Image.open(R"G:\AutoPoker\data\restart2.png")
+        try:
+            pgui.locateOnScreen(restart, region=(110, 275, 290, 55), confidence=0.950, grayscale=True)
+            print("STARTでポーカーを始めます")
+            start()
+            ORDER = 1
+        except:
+            pass
+    if ORDER == 0:
+        restart = Image.open(R"G:\AutoPoker\data\restart3.png")
+        try:
+            pgui.locateOnScreen(restart, region=(90, 275, 330, 55), confidence=0.950, grayscale=True)
+            print("STARTで再度ポーカーを始めます")
+            start()
+            ORDER = 1
+        except:
+            pass
+    if ORDER == 0:
+        order()
+    #ループ処理
+    if ORDER == 0:
+        order()
+
+#リストとフラグの初期化
+def flag():
+    global hand
+    hand.clear()
+    global JOKER
+    JOKER = 0
+    global FIN
+    FIN = 0
+
 hand = []
 JOKER = 0
 FIN = 0
 
-check() #存在するカードの情報をhandに加える
-joker() #ジョーカーが存在した場合handから削除し、JOKERフラグを立てる
-flush() #フラッシュ期待ハンドの対応
-if FIN == 0:
-    pair()  #スリーペア期待ハンドの対応
+for c in range(50):
+    print("try", c+1)
+    flag()
+    order()
